dc3467d25f090b4a4293da881b968509e9e81227
bhavnachauhann <your-email@example.com>
2024-10-12 20:06:32 +0530

    both

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -5,171 +5,171 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import './pagesfinance.css'
 
 const About = () => {
-  return (
-    <>
+	return (
+		<>
 
-<div class="hero overlay sec blogsec ">
-		
-		<div class="container">
-    <h1 class="heading text-white mb-3 aos-init aos-animate textbold" data-aos="fade-up">About Us</h1>
-		</div>
-	</div>
-{/* banner end */}
+			<div class="hero overlay sec blogsec ">
 
-    <div class="container-fluid">
-  <div class="row">
-    <div class="col-6 col-sm-12 aboutbg">
-      
-    </div>
-    <div class="col-6 col-sm-12">
-    <div class="  text p-5">
-				<h2 class="heading text-primary mb-3 textbold ">We are trusted by more than 5,000 clients</h2>
-				<p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-				{/* <p><a href="#" class="btn btn-outline-primary py-2">Read more</a></p> */}
+				<div class="container">
+					<h1 class="heading text-white mb-3 aos-init aos-animate textbold" data-aos="fade-up">About Us</h1>
+				</div>
 			</div>
-    </div>
-  
+			{/* banner end */}
 
- 
-    <div class="col-6 col-sm-12">
-    <div class="text p-5">
-				<h2 class="heading text-primary mb-3 textbold">We are trusted by more than 5,000 clients</h2>
-				<p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-				{/* <p><a href="#" class="btn btn-outline-primary py-2">Read more</a></p> */}
-			</div>
-    </div>
-    <div class="col-6  col-sm-12 aboutbg2">
-   
-    </div>
-	</div>
-	</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-12 aboutbg">
 
-{/*  sec-2 */}
-
-
-
-    <div class="section sec-features feature sec">
-		<div class="container my-2">
-			<div class="row g-5">
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="0">
-					<div class="feature d-flex">
-						<span class="bi-bag-check-fill icon  px-4"> </span>
-						<div>
-							<h3>Build financial</h3>
-							<p className='textwhite'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-						</div>
 					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-					<div class="feature d-flex">
-						<span class="bi-wallet-fill icon  px-4"></span>
-						<div>
-							<h3>Invest for the future</h3>
-							<p className='textwhite'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-					<div class="feature d-flex">
-						<span class="bi-pie-chart-fill icon  px-4"></span>
-						<div>
-							<h3>Responsible banking</h3>
-							<p className='textwhite'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	 {/* sec-3 */}
-	 <div class="section">
-		<div class="container">
-			
-			<div class="row mb-5 mt-5">
-				<div class="col-lg-5 mx-auto text-center aos-init aos-animate" data-aos="fade-up">
-					<h2 class="heading text-primary textbold">Our Team</h2>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="0">
-					<img src="images/person_1.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3"/>
-					<h5 class="text-black">James Griffin</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-				<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-					<img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3"/>
-					<h5 class="text-black">Claire Smith</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-				<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-					<img src="images/person_3.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3"/>
-					<h5 class="text-black">Jessica Wilson</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-
-				<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="0">
-					<img src="images/person_4.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3"/>
-					<h5 class="text-black">William Anderson</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-				<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-					<img src="images/person_5.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3"/>
-					<h5 class="text-black">Julie Harvey</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-				<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-					<img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3"/>
-					<h5 class="text-black">Shana Clarkson</h5>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
-			</div>
-
-		</div>
-	</div>
-
-	{/* sec-4 */}
-
-	<div class="section sec ">
-		<div class="container mt-5">
-			<div class="row justify-content-between">
-				<div class="col-lg-7 mb-4 mb-lg-0">
-					<img src="images/img-3.jpg" alt="Image" class="img-fluid rounded"></img>
-				</div>
-				<div class="col-lg-4 ps-lg-2">
-					<div class="mb-5">
-						<h2 class="text-black h4">Make payment fast and smooth.</h2>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-					</div>
-					<div class="d-flex mb-3 service-alt">
-						<div>
-							<span class="bi-wallet-fill me-4  iconf p-3"></span>
-						</div>
-						<div>
-							<h3>Build financial</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+					<div class="col-lg-6 col-md-6 col-sm-12">
+						<div class="  text p-5">
+							<h2 class="heading text-primary mb-3 textbold ">We are trusted by more than 5,000 clients</h2>
+							<p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							{/* <p><a href="#" class="btn btn-outline-primary py-2">Read more</a></p> */}
 						</div>
 					</div>
 
-					<div class="d-flex mb-3 service-alt">
-						<div>
-							<span class="bi-pie-chart-fill me-4 iconf p-3"></span>
+
+
+					<div class="col-lg-6 col-md-6 col-sm-12 order-2 order-md-1">
+						<div class="text p-5">
+							<h2 class="heading text-primary mb-3 textbold">We are trusted by more than 5,000 clients</h2>
+							<p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							{/* <p><a href="#" class="btn btn-outline-primary py-2">Read more</a></p> */}
 						</div>
-						<div>
-							<h3>Invest for the future</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-12 order-1 order-md-2 aboutbg2">
+
+					</div>
+				</div>
+			</div>
+
+			{/*  sec-2 */}
+
+
+
+			<div class="section sec-features feature sec">
+				<div class="container my-2">
+					<div class="row g-5">
+						<div class="col-12 col-sm-6 col-md-6 col-lg-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="0">
+							<div class="feature d-flex">
+								<span class="bi-bag-check-fill icon  px-4"> </span>
+								<div>
+									<h3>Build financial</h3>
+									<p className='textwhite'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>
+							</div>
+						</div>
+						<div class="col-12 col-sm-6 col-md-6 col-lg-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+							<div class="feature d-flex">
+								<span class="bi-wallet-fill icon  px-4"></span>
+								<div>
+									<h3>Invest for the future</h3>
+									<p className='textwhite'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>
+							</div>
+						</div>
+						<div class="col-12 col-sm-6 col-md-6 col-lg-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+							<div class="feature d-flex">
+								<span class="bi-pie-chart-fill icon  px-4"></span>
+								<div>
+									<h3>Responsible banking</h3>
+									<p className='textwhite'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* sec-3 */}
+			<div class="section">
+				<div class="container">
+
+					<div class="row mb-5 mt-5">
+						<div class="col-lg-5 mx-auto text-center aos-init aos-animate" data-aos="fade-up">
+							<h2 class="heading text-primary textbold">Our Team</h2>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="0">
+							<img src="images/person_1.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3" />
+							<h5 class="text-black">James Griffin</h5>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+						<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+							<img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3" />
+							<h5 class="text-black">Claire Smith</h5>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+						<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+							<img src="images/person_3.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3" />
+							<h5 class="text-black">Jessica Wilson</h5>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+
+						<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="0">
+							<img src="images/person_4.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3" />
+							<h5 class="text-black">William Anderson</h5>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+						<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+							<img src="images/person_5.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3" />
+							<h5 class="text-black">Julie Harvey</h5>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+						<div class="col-lg-4 mb-4 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+							<img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3" />
+							<h5 class="text-black">Shana Clarkson</h5>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
 						</div>
 					</div>
 
 				</div>
-
 			</div>
-		</div>
-	</div>
-    </>
-  )
+
+			{/* sec-4 */}
+
+			<div class="section sec ">
+				<div class="container mt-5">
+					<div class="row justify-content-between">
+						<div class="col-lg-7 mb-4 mb-lg-0">
+							<img src="images/img-3.jpg" alt="Image" class="img-fluid rounded"></img>
+						</div>
+						<div class="col-lg-4 ps-lg-2">
+							<div class="mb-5">
+								<h2 class="text-black h4">Make payment fast and smooth.</h2>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+							</div>
+							<div class="d-flex mb-3 service-alt">
+								<div>
+									<span class="bi-wallet-fill me-4  iconf p-3"></span>
+								</div>
+								<div>
+									<h3>Build financial</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								</div>
+							</div>
+
+							<div class="d-flex mb-3 service-alt">
+								<div>
+									<span class="bi-pie-chart-fill me-4 iconf p-3"></span>
+								</div>
+								<div>
+									<h3>Invest for the future</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default About
